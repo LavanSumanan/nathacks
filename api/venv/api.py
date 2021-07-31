21b13606076fefed2954d0db6e7703b40472710b
Ivan Yu <ivanyu327@gmail.com>
2021-07-31 01:22:05 -0400

    login functions

--- api/venv/api.py
+++ api/venv/api.py
@@ -1,9 +1,31 @@
-import time
-from flask import Flask
+from flask import Flask,request
+import login as login
 
 app = Flask(__name__)
 
 
-@app.route('/login')
-def validate_user(userdata):
-    return {'time': time.time()}
+@app.route('/')
+def test():
+    return "<p>honk</p>"
+
+@app.route('/login', methods=["POST","GET"])
+def login():
+    if request.method == "POST": #If theres actual things passed into the form
+        
+        username = str(request.form["username"])
+        password = str(request.form["password"])
+
+        print (login.login(username,password)) 
+        return login.login(username,password) 
+
+
+@app.route('/signup', methods=["POST","GET"])
+def signup():
+    if request.method == "POST": #If theres actual things passed into the form
+        
+        username = str(request.form["username"])
+        password = str(request.form["password"])
+
+        print (login.signUp(username,password)) 
+        return login.signUp(username,password) 
+
