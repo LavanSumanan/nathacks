e1694593758f4f4caa853d3ac99ce245e88f2e56
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-07-31 15:56:54 -0400

    added pyqt5 boilerplates

--- api/venv/api.py
+++ api/venv/api.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request
 import login as login
 
 app = Flask(__name__)
@@ -8,24 +8,24 @@ app = Flask(__name__)
 def test():
     return "<p>honk</p>"
 
-@app.route('/login', methods=["POST","GET"])
+
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    if request.method == "POST": #If theres actual things passed into the form
-        
+    if request.method == "POST":  # If theres actual things passed into the form
+
         username = str(request.form["username"])
         password = str(request.form["password"])
 
-        print (login.login(username,password)) 
-        return login.login(username,password) 
+        print(login.login(username, password))
+        return login.login(username, password)
 
 
-@app.route('/signup', methods=["POST","GET"])
+@app.route('/signup', methods=["POST", "GET"])
 def signup():
-    if request.method == "POST": #If theres actual things passed into the form
-        
+    if request.method == "POST":  # If theres actual things passed into the form
+
         username = str(request.form["username"])
         password = str(request.form["password"])
 
-        print (login.signUp(username,password)) 
-        return login.signUp(username,password) 
-
+        print(login.signUp(username, password))
+        return login.signUp(username, password)
--- api/venv/login.py
+++ api/venv/login.py
@@ -1,31 +1,31 @@
 from pymongo import MongoClient
 
-#MONGO STUFF --> DO NOT LEAK THIS KEY PLEASE
-cluster=MongoClient("mongodb+srv://BrainHyonk:BrainHyonkPassword@cluster.b3id9.mongodb.net/Nat_Hack?retryWrites=true&w=majority")
+# MONGO STUFF --> DO NOT LEAK THIS KEY PLEASE
+cluster = MongoClient(
+    "mongodb+srv://BrainHyonk:BrainHyonkPassword@cluster.b3id9.mongodb.net/Nat_Hack?retryWrites=true&w=majority")
 database = cluster["Data"]
 
-def login(username,password):
+
+def login(username, password):
     if username in database.list_collection_names():
-        userPost = database[username].find_one({"_id":"Login Info"},{"Password":1})
+        userPost = database[username].find_one(
+            {"_id": "Login Info"}, {"Password": 1})
         if password == userPost["Password"]:
-            return {"login":"success"} #hooray!
+            return {"login": "success"}  # hooray!
         else:
-            return {"login":"pw"} #wrong pw
+            return {"login": "pw"}  # wrong pw
     else:
-        return {"login":"dne"} #account doesn't exist
+        return {"login": "dne"}  # account doesn't exist
 
 
-def signUp(username,password):
+def signUp(username, password):
     if not username in database.list_collection_names():
         userInitPost = {
-            "_id":"Login Info",
-            "Username":username,
-            "Password":password,
+            "_id": "Login Info",
+            "Username": username,
+            "Password": password,
         }
         database[username].insert_one(userInitPost)
-        return {"signUp":"success"} #hooray new account posted!
+        return {"signUp": "success"}  # hooray new account posted!
     else:
-        return {"signUp":"exists"} #Username exists
-
-
-
+        return {"signUp": "exists"}  # Username exists
